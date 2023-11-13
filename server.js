069e76ccc4bbd7c1cd2738f781dee7be8604fdc0
Roni Seppälä <roni.o.seppala@gmail.com>
2023-11-13 11:56:54 +0200

    added first feature

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 //comments
+const helloJson = {
+    msg: "Hello world"
+}
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req,res) => {
+    res.send(helloJson)
+})
+
+app.listen(3000, () => console.log("server on"))
